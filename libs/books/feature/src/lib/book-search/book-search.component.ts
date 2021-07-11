f070ccede565d60deaa76fb3412f8c03e433fd8f
Milan Mishra <milan.mishra@infosys.com>
2021-07-11 21:15:09 +0530

    instant search feature

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
@@ -6,20 +6,30 @@ import {
   getAllBooks,
   searchBooks,
 } from '@tmo/books/data-access';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { Book, Constants } from '@tmo/shared/models';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  takeUntil,
+} from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent {
+export class BookSearchComponent implements OnInit, OnDestroy {
   constants = Constants;
   books$ = this.store.select(getAllBooks);
 
+  public instantSearchText: string;
+  private componentDestroyed: Subject<boolean> = new Subject();
+
   searchForm = this.fb.group({
-    term: new FormControl(null, [Validators.required]),
+    term: ''
   });
 
   constructor(
@@ -31,18 +41,37 @@ export class BookSearchComponent {
     return this.searchForm.value.term;
   }
 
+  ngOnInit(): void {
+    this.searchForm.valueChanges
+      .pipe(
+        map((val) => val.term),
+        debounceTime(500),
+        distinctUntilChanged(),
+        takeUntil(this.componentDestroyed)
+      )
+      .subscribe((searchTerm) => {
+        this.instantSearchText = searchTerm;
+        this.searchBooks();
+      });
+  }
+
   addBookToReadingList = (book: Book) => {
     this.store.dispatch(addToReadingList({ book }));
-  }
+  };
 
   searchExample = () => {
     this.searchForm.controls.term.setValue('javascript');
     this.searchBooks();
-  }
+  };
 
   searchBooks = () => {
     this.searchForm.value.term
       ? this.store.dispatch(searchBooks({ term: this.searchTerm }))
       : this.store.dispatch(clearSearch());
-  }
+  };
+
+  ngOnDestroy = () => {
+    this.componentDestroyed.next(true);
+    this.componentDestroyed.complete();
+  };
 }
